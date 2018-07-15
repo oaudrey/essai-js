679eba1d97f66e9a665c8a003a0e8713471d1aff
alexandrebk <alexandre.b2506@gmail.com>
2018-07-15 12:06:52 +0200

    API KEY

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
   var mymap = L.map('mapid').setView([45.764043,4.835658999999964], 13);
 
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + ENV['API_KEY'], {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -12,3 +12,4 @@
     .bindPopup("<b>Bonjour Lyon !</b><br />").openPopup();
 
 // ENV['LEVEL_SEED']
+// 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
