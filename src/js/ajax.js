e6681973f700a814baae8ddc6b11b01ee5851efe
alexandrebk <alexandre.b2506@gmail.com>
2018-08-10 11:59:55 +0200

    ToDoList

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -5,7 +5,7 @@ function ajaxGet(url, callback) {
     req.open("GET", url);
     req.addEventListener("load", function () {
         if (req.status >= 200 && req.status < 400) {
-            // Call of callback function 
+            // Call of callback function
             callback(req.responseText);
         } else {
             console.error(req.status + " " + req.statusText + " " + url);
@@ -16,3 +16,15 @@ function ajaxGet(url, callback) {
     });
     req.send(null);
 }
+
+// En ES6 on utilise plutôt fetch pour faire des requetes AJAX
+// La doc est ici https://developer.mozilla.org/fr/docs/Web/API/Fetch_API/Using_Fetch
+// Tu récupères les données de l'API et on va en faire des Objets de station Vélov
+
+// Exemple:
+
+fetch("https://swapi.co/api/people/")
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data);
+  });
--- src/js/station.js
+++ src/js/station.js
@@ -1,3 +1,10 @@
+// Prendre le meme wording que l'API JCDECAUX
+//  "bike_stands": 20,
+//  "available_bike_stands": 15,
+//  "available_bikes": 5,
+// On va ajouter un ID
+// "number": 123,
+
 function StationVelov (latitude,longitude,espaceDispo,nombreVelo) {
 this.latitude = latitude;
 this.longitude = longitude;
@@ -5,18 +12,15 @@ this.espaceDispo = espaceDispo;
 this.nombreVelo = nombreVelo;
 }
 
+// Créer des objets avec les données de l'API une fois qu'on a récupéré les données
+
 var stationVelov1 = new StationVelov (45.7712918,4.828083099999958,20,0);
 var stationVelov2 = new StationVelov (45.7699424,4.80371839999998,20,3);
 var stationVelov3 = new StationVelov (45.7477113,4.824099899999965,15,10);
 var stationVelov4 = new StationVelov (45.78095219999999,4.824349100000063,30,2);
 
-
-// Dans var StationVelov il faudrait plutot mettre tous les objets définis au dessus (l.8 à l.11).
-// En fait tu repetes 2 fois la même chose dans ton code.
-// par exemple:
-// var StationVelov = [StationVelov1, StationVelov2, etc...]
-
 var StationVelov =[stationVelov1, stationVelov2, stationVelov3, stationVelov4];
+
 
 // Ajout d'un icone
 var Icon = L.icon({
@@ -25,7 +29,7 @@ var Icon = L.icon({
             popupAnchor:  [0, -20]});
 
 // Boucle pour afficher les markers
-
+// on ajoute un texte au marker avec le nombre de bornes et  les vélos disponibles et l'ID
 StationVelov.forEach(function(element) {
   console.log(element);
   console.log(element.latitude);
@@ -36,19 +40,16 @@ L.marker([element.latitude, element.longitude], {icon: Icon}).addTo(mymap);
 });
 
 
-  // Maintenant que tu as element tu peux appeler la latitude et longitude
-  //  Avec ces 2 données tu construit le marker  en remplacant StationVelov1.latitude par element.latitude etc..
-
-
-// Pour mettre les infos dans l'HTML c'est un peu plus compliqué on va procéder par étape.
-// D'abord tu vas faire en sorte que qd on clique sur un station, tu tu récupères l'element qui sur lequel on a cliqué puis tu l'affiches dans la console
-// Je te laisse chercher sur internet comment faire
-//  Essaie de googeliser ça en mettant de l'anglais de préférence ;)
 
 mymap.addEventListener('click', function(){
   console.log($(this));
 
 });
+
+// Une fois que la personne a cliqué on récupère le texte sur le marker.
+// Ensuite on sléectionne le composant HTML avec querySelector --> https://developer.mozilla.org/fr/docs/Web/API/Document/querySelector
+// ou avec getElementByID --> https://developer.mozilla.org/fr/docs/Web/API/Document/getElementById
+// On ajoute du texte à ce composant avec innerHTML -->
 
 
 
